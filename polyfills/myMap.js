da28914414e0a6ef60183969d2c81318e0c5cb8a
Saqib <iamsaqib24@gmail.com>
2023-07-10 10:49:23 +0530

    map-method-polyfill

---
+++ polyfills/myMap.js
@@ -0,0 +1,19 @@
+const array = [1, 2, 3, 4, 5];
+
+function multiplyBy2(num) {
+    return num * 2;
+}
+
+Array.prototype.myMap = function(callback) {
+    let output = [];
+    for(let i = 0; i < this.length; i++) {
+        output.push(callback(this[i]));
+    }
+    return output;
+}
+
+const newArray = array.myMap(multiplyBy2);
+console.log(newArray);
+
+console.log("Using myMap method:", array.myMap(item => item * 3));
+console.log("Using default map method:", array.map(item => item * 3));
