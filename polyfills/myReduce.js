817b617bcfb3a53bf9be33017cbf0770e97cc7c6
Saqib <iamsaqib24@gmail.com>
2023-07-24 23:15:49 +0530

    polyfills

--- polyfills/myReduce.js
+++ polyfills/myReduce.js
@@ -1,18 +1,28 @@
-Array.prototype.myReduce = function(callback, initialValue) {
-    let accumulator = initialValue;
+Array.prototype.myReduce = function (callback, initialValue) {
+  let accumulator = initialValue;
 
-    for(let i = 0; i < this.length; i++) {
-        if(accumulator === 0 && accumulator === 'undefined'){
-            accumulator = this[i];
-        } else {
-            accumulator = callback(accumulator, this[i], i, this);
-        }
+  for (let i = 0; i < this.length; i++) {
+    if (accumulator === 0 && accumulator === "undefined") {
+      accumulator = this[i];
+    } else {
+      accumulator = callback(accumulator, this[i], i, this);
     }
-    return accumulator;
-}
+  }
+  return accumulator;
+};
 
 const array = [1, 2, 3, 4, 5, 6];
 
-console.log("Using myReduce method: ", array.myReduce((acc, curr, i, arr) => { return acc + curr }, 0));
+console.log(
+  "Using myReduce method: ",
+  array.myReduce((acc, curr, i, arr) => {
+    return acc + curr;
+  }, 0)
+);
 
-console.log("Using default Reduce method: ", array.reduce((acc, curr, i, arr) => { return acc + curr }, 0));
+console.log(
+  "Using default Reduce method: ",
+  array.reduce((acc, curr, i, arr) => {
+    return acc + curr;
+  }, 0)
+);
